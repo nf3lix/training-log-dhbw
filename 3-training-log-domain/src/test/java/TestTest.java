87033f52b24afc7667ad3b7d8064e31888d63fb5
nf3lix <felix01.schneider@gmail.com>
2022-02-19 22:17:17 +0100

    Update .gitignore and add Test dependencies

---
+++ 3-training-log-domain/src/test/java/TestTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TestTest {
+
+    @Test
+    public void test() {
+        Assertions.assertEquals(2, 1 + 1);
+    }
+
+}
