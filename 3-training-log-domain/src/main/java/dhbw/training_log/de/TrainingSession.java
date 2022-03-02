6ec7edbf1496fc5996e0d61bcc6f2dcec949178d
nf3lix <felix01.schneider@gmail.com>
2022-03-02 18:52:05 +0100

    Add TrainingSession Entity Class

---
+++ 3-training-log-domain/src/main/java/dhbw/training_log/de/TrainingSession.java
@@ -0,0 +1,26 @@
+package dhbw.training_log.de;
+
+import dhbw.training_log.de.description.Description;
+import dhbw.training_log.de.distance.Distance;
+import dhbw.training_log.de.time.SessionTime;
+import dhbw.training_log.de.training_session_id.TrainingSessionId;
+import dhbw.training_log.de.training_session_type.TrainingSessionType;
+
+public class TrainingSession {
+
+    private final TrainingSessionId id;
+    private Distance distance;
+    private SessionTime time;
+    private Description description;
+    private TrainingSessionType type;
+
+    public TrainingSession(final TrainingSessionId id, final Distance distance, final SessionTime time,
+                           final Description description, final TrainingSessionType type) {
+        this.id = id;
+        this.distance = distance;
+        this.time = time;
+        this.description = description;
+        this.type = type;
+    }
+
+}
