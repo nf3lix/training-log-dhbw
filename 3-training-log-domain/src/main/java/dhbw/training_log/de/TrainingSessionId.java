c7095ab1679043b37e79a180ab47ea0573859eb6
nf3lix <felix01.schneider@gmail.com>
2022-03-02 17:54:17 +0100

    Add TrainingSessionId value object

---
+++ 3-training-log-domain/src/main/java/dhbw/training_log/de/TrainingSessionId.java
@@ -0,0 +1,27 @@
+package dhbw.training_log.de;
+
+import java.util.Objects;
+import java.util.UUID;
+
+public final class TrainingSessionId {
+
+    private final UUID uuid;
+
+    public TrainingSessionId(final UUID uuid) {
+        this.uuid = uuid;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        TrainingSessionId that = (TrainingSessionId) o;
+        return Objects.equals(uuid, that.uuid);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(uuid);
+    }
+
+}
---
+++ 3-training-log-domain/src/test/java/dhbw/training_log/de/TrainingSessionIdTest.java
@@ -0,0 +1,27 @@
+package dhbw.training_log.de;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
+import java.util.function.Supplier;
+
+import static dhbw.training_log.de.test_utils.ValueObjectTest.performValueObjectTest;
+
+public class TrainingSessionIdTest {
+
+    @Test
+    public void valueObjectBehavior() {
+        final List<Supplier<TrainingSessionId>> trainingSessionIds = Arrays.asList(
+                () -> new TrainingSessionId(uuid("af2f909b-50cb-4fc5-aceb-c9fdc4699c27")),
+                () -> new TrainingSessionId(uuid("e79b3ff0-530f-48d3-a8dd-e066fd2190fd")),
+                () -> new TrainingSessionId(uuid("b206633a-0516-4d8b-844a-3e4b68557886")));
+        performValueObjectTest(trainingSessionIds);
+    }
+
+    private UUID uuid(final String fromString) {
+        return UUID.fromString(fromString);
+    }
+
+}
