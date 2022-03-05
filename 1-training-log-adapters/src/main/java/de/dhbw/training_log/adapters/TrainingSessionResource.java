e2a071b75691f4632feed8d1d334669b6c2936d0
nf3lix <felix01.schneider@gmail.com>
2022-03-05 21:40:58 +0100

    Add TrainingResourceMapper

--- 1-training-log-adapters/src/main/java/de/dhbw/training_log/adapters/TrainingSessionResource.java
+++ 1-training-log-adapters/src/main/java/de/dhbw/training_log/adapters/TrainingSessionResource.java
@@ -1,13 +1,49 @@
 package de.dhbw.training_log.adapters;
 
+import de.dhbw.training_log.adapters.training_session_ressource.DistanceResource;
 import de.dhbw.training_log.adapters.training_session_ressource.SessionTimeResource;
+import dhbw.training_log.de.training_session_type.TrainingSessionType;
+
+import java.util.UUID;
 
 public final class TrainingSessionResource {
 
+    private final UUID id;
+    private final DistanceResource distance;
     private final SessionTimeResource sessionTime;
+    private final String description;
+    private final TrainingSessionType type;
 
-    public TrainingSessionResource(SessionTimeResource sessionTime) {
+    public TrainingSessionResource(final UUID id,
+                                   final DistanceResource distance,
+                                   final SessionTimeResource sessionTime,
+                                   final String description,
+                                   final TrainingSessionType type) {
+        this.id = id;
+        this.distance = distance;
         this.sessionTime = sessionTime;
+        this.description = description;
+        this.type = type;
+    }
+
+    public UUID id() {
+        return id;
+    }
+
+    public DistanceResource distance() {
+        return distance;
+    }
+
+    public SessionTimeResource sessionTime() {
+        return sessionTime;
+    }
+
+    public String description() {
+        return description;
+    }
+
+    public TrainingSessionType type() {
+        return type;
     }
 
 }
--- 1-training-log-adapters/src/main/java/de/dhbw/training_log/adapters/TrainingSessionResourceMapper.java
+++ 1-training-log-adapters/src/main/java/de/dhbw/training_log/adapters/TrainingSessionResourceMapper.java
@@ -2,10 +2,28 @@ package de.dhbw.training_log.adapters;
 
 import de.dhbw.training_log.adapters.training_session_ressource.DistanceResource;
 import de.dhbw.training_log.adapters.training_session_ressource.SessionTimeResource;
+import dhbw.training_log.de.TrainingSession;
+import dhbw.training_log.de.description.Description;
+import dhbw.training_log.de.distance.Distance;
+import dhbw.training_log.de.time.Minutes;
+import dhbw.training_log.de.time.Seconds;
+import dhbw.training_log.de.time.SessionTime;
+import dhbw.training_log.de.training_session_id.TrainingSessionId;
 
 import java.util.function.Function;
 
-public class TrainingSessionResourceMapper {
+public class TrainingSessionResourceMapper implements Function<TrainingSessionResource, TrainingSession> {
+
+    @Override
+    public TrainingSession apply(TrainingSessionResource resource) {
+        return new TrainingSession(
+                new TrainingSessionId(resource.id()),
+                new Distance(resource.distance().distance(), resource.distance().unit()),
+                new SessionTime(new Minutes(resource.sessionTime().minutes()), new Seconds(resource.sessionTime().seconds())),
+                new Description(resource.description()),
+                resource.type()
+        );
+    }
 
     public static class SessionTimeMapper implements Function<String, SessionTimeResource> {
         @Override
