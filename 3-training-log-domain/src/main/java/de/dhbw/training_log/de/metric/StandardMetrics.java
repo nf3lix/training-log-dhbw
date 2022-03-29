cb32e4ca6d7e4d290d817f7a78ba0aae98bf32a3
nf3lix <felix01.schneider@gmail.com>
2022-03-29 19:35:02 +0200

    Refactor: extract method for computing minimum/maximum of session time and distance

--- 3-training-log-domain/src/main/java/de/dhbw/training_log/de/metric/StandardMetrics.java
+++ 3-training-log-domain/src/main/java/de/dhbw/training_log/de/metric/StandardMetrics.java
@@ -29,20 +29,14 @@ public final class StandardMetrics {
     public static final class MaxDistanceMetric extends Metric {
         @Override
         public MetricResult compute(List<Session> list) {
-            final List<Distance> distances = list.stream().map(Session::distance).collect(Collectors.toList());
-            final Map<Distance, Session> map = list.stream().collect(Collectors.toMap(Session::distance, Function.identity()));
-            final Distance maxDistance = new MAX<Distance>().compute(distances);
-            return new MetricResult(map.get(maxDistance));
+            return computeMaximumFor(list, Session::distance);
         }
     }
 
     public static final class MinDistanceMetric extends Metric {
         @Override
         public MetricResult compute(List<Session> list) {
-            final List<Distance> distances = list.stream().map(Session::distance).collect(Collectors.toList());
-            final Map<Distance, Session> sessions = list.stream().collect(Collectors.toMap(Session::distance, Function.identity()));
-            final Distance minDistance = new MIN<Distance>().compute(distances);
-            return new MetricResult(sessions.get(minDistance));
+            return computeMinimumFor(list, Session::distance);
         }
     }
 
@@ -58,20 +52,14 @@ public final class StandardMetrics {
     public static final class MaxSessionTimeMetric extends Metric {
         @Override
         public MetricResult compute(List<Session> list) {
-            final List<SessionTime> sessionTimes = list.stream().map(Session::time).collect(Collectors.toList());
-            final Map<SessionTime, Session> sessions = list.stream().collect(Collectors.toMap(Session::time, Function.identity()));
-            final SessionTime maxSessionTime = new MAX<SessionTime>().compute(sessionTimes);
-            return new MetricResult(sessions.get(maxSessionTime));
+            return computeMaximumFor(list, Session::time);
         }
     }
 
     public static final class MinSessionTimeMetric extends Metric {
         @Override
         public MetricResult compute(List<Session> list) {
-            final List<SessionTime> sessionTimes = list.stream().map(Session::time).collect(Collectors.toList());
-            final Map<SessionTime, Session> sessions = list.stream().collect(Collectors.toMap(Session::time, Function.identity()));
-            final SessionTime minSessionTime = new MIN<SessionTime>().compute(sessionTimes);
-            return new MetricResult(sessions.get(minSessionTime));
+            return computeMinimumFor(list, Session::time);
         }
     }
 
@@ -107,6 +95,21 @@ public final class StandardMetrics {
             return new AggregateFunction.SUM<SessionTime>().compute(sessionTimes);
         }
 
+    }
+
+    private static <T extends Comparable<T>> Metric.MetricResult computeMaximumFor(final List<Session> sessionList, final Function<? super Session, ? extends T> mapper) {
+        return computeAggregateFor(sessionList, mapper, new MAX<>());
+    }
+
+    private static <T extends Comparable<T>> Metric.MetricResult computeMinimumFor(final List<Session> sessionList, final Function<? super Session, ? extends T> mapper) {
+        return computeAggregateFor(sessionList, mapper, new MIN<>());
+    }
+
+    private static <T extends Comparable<T>> Metric.MetricResult computeAggregateFor(final List<Session> sessionList, final Function<? super Session, ? extends T> mapper, final AggregateFunction<T, T> aggregateFunction) {
+        final List<T> itemList = sessionList.stream().map(mapper).collect(Collectors.toList());
+        final Map<T, Session> sessions = sessionList.stream().collect(Collectors.toMap(mapper, Function.identity()));
+        final T selectedItem = aggregateFunction.compute(itemList);
+        return new Metric.MetricResult(sessions.get(selectedItem));
     }
 
 }
