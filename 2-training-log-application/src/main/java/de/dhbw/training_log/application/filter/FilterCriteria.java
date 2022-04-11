c1fa97b8ea62e9bde0531375a4eb3f47c8517542
felix <felix01.schneider@gmail.com>
2022-04-11 15:23:25 +0200

    Add FilterCriteria class

--- 2-training-log-application/src/main/java/de/dhbw/training_log/application/filter/FilterCriteria.java
+++ 2-training-log-application/src/main/java/de/dhbw/training_log/application/filter/FilterCriteria.java
@@ -1,7 +1,36 @@
 package de.dhbw.training_log.application.filter;
 
-public class FilterCriteria {
+import de.dhbw.training_log.de.comparison_operator.ComparisonOperator;
+import de.dhbw.training_log.de.session.Session;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
 
+public class FilterCriteria<T extends Comparable<T>> {
+
+    private final Function<? super Session, ? extends T> mapper;
+    private final ComparisonOperator operator;
+    private final T comparedObject;
+
+    public FilterCriteria(
+            final Function<? super Session, ? extends T> mapper,
+            final ComparisonOperator operator,
+            final T comparedObject) {
+        this.mapper = mapper;
+        this.operator = operator;
+        this.comparedObject = comparedObject;
+    }
+
+    public List<Session> apply(final List<Session> sessionList) {
+        final List<Session> resultList = new ArrayList<>();
+        for(Session currentSession : sessionList) {
+            final T currentObject = mapper.apply(currentSession);
+            if(operator.matches(currentObject, comparedObject)) {
+                resultList.add(currentSession);
+            }
+        }
+        return resultList;
+    }
 
 }
