8e6c58164ac23528f7c7ad3a948f714ef74a6652
nf3lix <felix01.schneider@gmail.com>
2022-04-13 19:44:48 +0200

    Add Filter Sessions use case class

--- 0-training-log-plugins/src/main/java/de/dhbw/training_log/plugins/usecases/UseCase.java
+++ 0-training-log-plugins/src/main/java/de/dhbw/training_log/plugins/usecases/UseCase.java
@@ -1,6 +1,7 @@
 package de.dhbw.training_log.plugins.usecases;
 
 import de.dhbw.training_log.plugins.usecases.create_session.CreateSession;
+import de.dhbw.training_log.plugins.usecases.filter_sessions.FilterSessions;
 import de.dhbw.training_log.plugins.usecases.generate_report.GenerateReport;
 import de.dhbw.training_log.plugins.usecases.read_session.ReadSession;
 import de.dhbw.training_log.de.session.SessionRepository;
@@ -13,7 +14,8 @@ public enum UseCase {
 
     CREATE_SESSION("1","Create new training session", new CreateSession()),
     READ_SESSION("2", "Show all training sessions", new ReadSession()),
-    GENERATE_REPORT("3", "Generate Report", new GenerateReport());
+    GENERATE_REPORT("3", "Generate Report", new GenerateReport()),
+    FILTER_SESSIONS("4", "Filter sessions", new FilterSessions());
 
     private final String mnemonic;
     private final String useCaseDescription;
---
+++ 0-training-log-plugins/src/main/java/de/dhbw/training_log/plugins/usecases/filter_sessions/FilterSessions.java
@@ -0,0 +1,13 @@
+package de.dhbw.training_log.plugins.usecases.filter_sessions;
+
+import de.dhbw.training_log.de.session.SessionRepository;
+import de.dhbw.training_log.plugins.usecases.UseCaseInitializer;
+
+public class FilterSessions extends UseCaseInitializer {
+
+    @Override
+    public void init(SessionRepository repository) {
+
+    }
+
+}
