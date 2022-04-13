68e3439247c943304ae2825569d621d7211ffc21
nf3lix <felix01.schneider@gmail.com>
2022-04-13 20:09:40 +0200

    Add string representations to comparison operators

--- 4-training-log-abstractioncode/src/main/java/de/dhbw/training_log/de/comparison_operator/ComparisonOperator.java
+++ 4-training-log-abstractioncode/src/main/java/de/dhbw/training_log/de/comparison_operator/ComparisonOperator.java
@@ -1,48 +1,59 @@
 package de.dhbw.training_log.de.comparison_operator;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public enum ComparisonOperator {
 
-    EQUAL_TO {
+    EQUAL_TO("=", "==") {
         @Override
         public <T extends Comparable<T>> boolean matches(T object1, T object2) {
             return object1.compareTo(object2) == 0;
         }
     },
 
-    LESS_THAN {
+    LESS_THAN("<") {
         @Override
         public <T extends Comparable<T>> boolean matches(T object1, T object2) {
             return object1.compareTo(object2) > 0;
         }
     },
 
-    GREATER_THAN {
+    GREATER_THAN(">") {
         @Override
         public <T extends Comparable<T>> boolean matches(T object1, T object2) {
             return object1.compareTo(object2) < 0;
         }
     },
 
-    LESS_OR_EQUAL {
+    LESS_OR_EQUAL("<=", "=<") {
         @Override
         public <T extends Comparable<T>> boolean matches(T object1, T object2) {
             return object1.compareTo(object2) >= 0;
         }
     },
 
-    GREATER_OR_EQUAL {
+    GREATER_OR_EQUAL(">=", "=>") {
         @Override
         public <T extends Comparable<T>> boolean matches(T object1, T object2) {
             return object1.compareTo(object2) <= 0;
         }
     },
 
-    NOT_EQUAL {
+    NOT_EQUAL("!=") {
         @Override
         public <T extends Comparable<T>> boolean matches(T object1, T object2) {
             return object1.compareTo(object2) != 0;
         }
     };
+
+    private final List<String> operatorStrings = new ArrayList<>();
+
+    ComparisonOperator(final String operatorString, final String... operatorStrings) {
+        this.operatorStrings.add(operatorString);
+        this.operatorStrings.addAll(Arrays.asList(operatorStrings));
+    }
 
     /**
      * for a given comparison operator ∘, return whether comparison operation "firstParam ∘ secondParam" is true
