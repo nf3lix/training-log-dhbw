0a66143fbf73e28eb7ca04b8587f852060aa46a5
nf3lix <felix01.schneider@gmail.com>
2022-05-21 09:49:40 +0200

    Refactor conversion of metric result to string

--- 1-training-log-adapters/src/main/java/de/dhbw/training_log/adapters/mapper/SessionResourceMapper.java
+++ 1-training-log-adapters/src/main/java/de/dhbw/training_log/adapters/mapper/SessionResourceMapper.java
@@ -1,35 +1,6 @@
 package de.dhbw.training_log.adapters.mapper;
 
-import de.dhbw.training_log.de.metric.Metric;
-import de.dhbw.training_log.de.session.Session;
-import de.dhbw.training_log.de.session.distance.Distance;
-import de.dhbw.training_log.de.session.session_date.SessionDate;
-import de.dhbw.training_log.de.session.time.SessionTime;
-
-import java.util.HashMap;
-import java.util.Map;
-
 public interface SessionResourceMapper<T, K> {
     T toResource(final Object domainModelObject);
     K toDomainModel(final T resource);
-
-    static SessionResourceMapper<?, ?> getMapper(Class<?> c) {
-        final Map<Class<?>, SessionResourceMapper<?, ?>> mappers = new HashMap<>();
-        mappers.put(Session.class, new SessionEntityMapper());
-        mappers.put(Distance.class, new DistanceMapper());
-        mappers.put(SessionDate.class, new SessionTimeMapper());
-        mappers.put(SessionTime.class, new SessionTimeMapper());
-        mappers.put(Metric.MetricResult.class, new MetricResultMapper());
-        return mappers.get(c);
-    }
-
-    /*
-    static SessionResourceMapper<?, ?> getValueMapperByClassName(final String className) {
-        final Map<String, SessionResourceMapper<?, ?>> mappers = new HashMap<>();
-        mappers.put(Distance.class.getName(), new DistanceMapper());
-        mappers.put(SessionDate.class.getName(), new SessionTimeMapper());
-        mappers.put(SessionTime.class.getName(), new SessionTimeMapper());
-        return mappers.get(className);
-    }*/
-
 }
--- 1-training-log-adapters/src/main/java/de/dhbw/training_log/adapters/resource/MetricResultResource.java
+++ 1-training-log-adapters/src/main/java/de/dhbw/training_log/adapters/resource/MetricResultResource.java
@@ -1,6 +1,10 @@
 package de.dhbw.training_log.adapters.resource;
 
-import de.dhbw.training_log.adapters.mapper.SessionResourceMapper;
+import de.dhbw.training_log.adapters.mapper.*;
+import de.dhbw.training_log.de.session.Session;
+import de.dhbw.training_log.de.session.distance.Distance;
+import de.dhbw.training_log.de.session.session_date.SessionDate;
+import de.dhbw.training_log.de.session.time.SessionTime;
 
 public class MetricResultResource {
 
@@ -24,11 +28,57 @@ public class MetricResultResource {
 
     @Override
     public String toString() {
-        String output = result.toString();
-        final SessionResourceMapper<?, ?> mapper = SessionResourceMapper.getMapper(resultClass);
-        if(mapper != null) {
-            output = mapper.toResource(resultClass.cast(result)).toString();
-        }
-        return this.metricName + " - " + output;
+        final String valueString = ObjectStringMapper.convertToString(this.result, this.resultClass);
+        return this.metricName + " - " + valueString;
     }
+
+    private enum ObjectStringMapper {
+
+        SESSION(Session.class) {
+            @Override
+            String convertValueToString(final Object value) {
+                return new SessionEntityMapper().toResource(value).toString();
+            }
+        },
+
+        DISTANCE(Distance.class) {
+            @Override
+            String convertValueToString(Object value) {
+                return new DistanceMapper().toResource(value).toString();
+            }
+        },
+
+        SESSION_DATE(SessionDate.class) {
+            @Override
+            String convertValueToString(Object value) {
+                return new SessionDateMapper().toResource(value).toString();
+            }
+        },
+
+        SESSION_TIME(SessionTime.class) {
+            @Override
+            String convertValueToString(Object value) {
+                return new SessionTimeMapper().toResource(value).toString();
+            }
+        };
+
+        private final Class<?> valueClass;
+
+        ObjectStringMapper(final Class<?> valueClass) {
+            this.valueClass = valueClass;
+        }
+
+        abstract String convertValueToString(Object value);
+
+        private static String convertToString(final Object value, final Class<?> valueClass) {
+            for(final ObjectStringMapper mapper : ObjectStringMapper.values()) {
+                if(valueClass == mapper.valueClass) {
+                    return mapper.convertValueToString(value);
+                }
+            }
+            return value.toString();
+        }
+
+    }
+
 }
