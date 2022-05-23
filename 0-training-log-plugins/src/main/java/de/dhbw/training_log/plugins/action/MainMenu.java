4bb61806ac460cb2dc6b8e3a04720a85d667b242
nf3lix <felix01.schneider@gmail.com>
2022-05-23 20:37:34 +0200

    Add UpdateSessionAction in Plugin layer

--- 0-training-log-plugins/src/main/java/de/dhbw/training_log/plugins/action/MainMenu.java
+++ 0-training-log-plugins/src/main/java/de/dhbw/training_log/plugins/action/MainMenu.java
@@ -1,10 +1,10 @@
 package de.dhbw.training_log.plugins.action;
 
 import de.dhbw.training_log.de.session.SessionRepository;
-import de.dhbw.training_log.plugins.persistence.SessionRepositoryImpl;
 import de.dhbw.training_log.plugins.usecases.crud_session.CreateSessionAction;
 import de.dhbw.training_log.plugins.usecases.crud_session.DeleteSessionAction;
 import de.dhbw.training_log.plugins.usecases.crud_session.ReadSessionAction;
+import de.dhbw.training_log.plugins.usecases.crud_session.UpdateSessionAction;
 import de.dhbw.training_log.plugins.usecases.filter_sessions.SearchSessionsAction;
 import de.dhbw.training_log.plugins.usecases.generate_report.GenerateReportAction;
 
@@ -14,9 +14,10 @@ public final class MainMenu extends ActionMenu {
         super("Main Menu");
         this.addUseCase("1", new CreateSessionAction(repository));
         this.addUseCase("2", new ReadSessionAction(repository));
-        this.addUseCase("3", new DeleteSessionAction(repository));
-        this.addUseCase("4", new GenerateReportAction(repository));
-        this.addUseCase("5", new SearchSessionsAction(repository));
+        this.addUseCase("3", new UpdateSessionAction(repository));
+        this.addUseCase("4", new DeleteSessionAction(repository));
+        this.addUseCase("5", new GenerateReportAction(repository));
+        this.addUseCase("6", new SearchSessionsAction(repository));
     }
 
 }
